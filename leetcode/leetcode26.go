2603fa02b8025e90083f18556df09e9295dfad10
tianyu.gu <tianyu.gu@easystack.cn>
2023-08-17 18:24:44 +0800

    leetcode26

--- leetcode/leetcode26.go
+++ leetcode/leetcode26.go
@@ -4,19 +4,15 @@ func RemoveDuplicates(nums []int) int {
 	var (
 		ptr0 int = 0
 		ptr1 int = 1
-		sum  int = 0
 	)
 
 	for ptr1 < len(nums) {
-		if nums[ptr0] == nums[ptr1] && sum < 2 {
+		if nums[ptr0] == nums[ptr1] {
 			ptr1++
 			sum++
 		} else {
 			ptr0++
 			nums[ptr0] = nums[ptr1]
-			if sum >= 2 {
-				sum--
-			}
 		}
 	}
 
