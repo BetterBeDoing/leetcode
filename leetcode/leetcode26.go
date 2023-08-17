1dd4da4bac6aca6bd4da12f56e17bf9e2409e1cd
tianyu.gu <tianyu.gu@easystack.cn>
2023-08-17 18:14:04 +0800

    leetcode26

---
+++ leetcode/leetcode26.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func RemoveDuplicates(nums []int) int {
+	var (
+		ptr0 int = 0
+		ptr1 int = 0
+	)
+
+	for ptr1 < len(nums) {
+		if nums[ptr0] == nums[ptr1] {
+			ptr1++
+		} else {
+			ptr0++
+			nums[ptr0] = nums[ptr1]
+		}
+	}
+
+	return ptr0 + 1
+}
