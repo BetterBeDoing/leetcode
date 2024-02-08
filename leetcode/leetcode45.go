c9896e46ba2373137a947ee3702fbfc8e4373fc4
tianyu.gu <tianyu.gu@easystack.cn>
2024-02-08 11:46:38 +0800

    lc45

---
+++ leetcode/leetcode45.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func jump(nums []int) int {
+	length := len(nums)
+	end := 0
+	maxPosition := 0
+	steps := 0
+	for i := 0; i < length-1; i++ {
+		maxPosition = max(maxPosition, i+nums[i])
+		if i == end {
+			end = maxPosition
+			steps++
+		}
+	}
+	return steps
+}
