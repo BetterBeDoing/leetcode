43ff589f50743cbbd3032ba5cac677734ad9fdbd
BeBetter <gutianyu9871@gmail.com>
2024-02-13 04:01:51 +0800

    lc42

--- leetcode/leetcode42.go
+++ leetcode/leetcode42.go
@@ -1,7 +1,39 @@
 package leetcode
 
-func trap(height []int) int {
+func trap(height []int) (ans int) {
 	n := len(height)
-	for 
-	
+	if n == 0 {
+		return
+	}
+
+	leftMax := make([]int, n)
+	leftMax[0] = height[0]
+	for i := 1; i < n; i++ {
+		leftMax[i] = max(leftMax[i-1], height[i])
+	}
+
+	rightMax := make([]int, n)
+	rightMax[n-1] = height[n-1]
+	for i := n - 2; i >= 0; i-- {
+		rightMax[i] = max(rightMax[i+1], height[i])
+	}
+
+	for i, h := range height {
+		ans += min(leftMax[i], rightMax[i]) - h
+	}
+	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
