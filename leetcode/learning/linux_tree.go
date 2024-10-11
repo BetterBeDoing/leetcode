1c5cd5cf6d3e9515d422e9a11ad04175b3c28d3b
BeBetter <gutianyu9871@gmail.com>
2024-10-11 20:52:46 +0800

    implement

--- leetcode/learning/linux_tree.go
+++ leetcode/learning/linux_tree.go
@@ -7,13 +7,13 @@ import (
 )
 
 // Tree is the function to print the tree structure of the current directory
-func Tree(fp string) {
-	filepath.Walk(fp, func(path string, info os.FileInfo, err error) error {
+func Tree(root string) {
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Println(err)
 			return nil
 		}
-		if path == fp {
+		if path == root {
 			fmt.Println(path)
 			return nil
 		}
